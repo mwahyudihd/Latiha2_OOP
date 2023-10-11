4efe715f52923eba489cb395e77c90e3e0108fc8
mwahyudihd <146799485+mwahyudihd@users.noreply.github.com>
2023-10-11 15:59:55 +0700

    Add files via upload

---
+++ Latihan2/Latihan2/HonorClass.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan2.Honor
+{
+    class HonorClass
+    {
+        public string? status;
+        public string? pendidikan;
+        public int inputSKS;
+        
+
+        //tunjangan fungsional
+        public int tFunctional()
+        {
+            if (status! == "Tetap" || status! == "tetap") {
+                return 325000;
+            }
+            else if (status! == "Tidak" || status == "tidak")
+            {
+                return 0;
+            }   
+            else { return 0; }
+        }
+
+        //uang transport
+        public int uTransport ()
+        {
+            if (status! == "Tetap" || status! == "tetap")
+            {
+                return 0;
+            }
+            else if (status! == "Tidak" || status! == "tidak") {
+                return 250000;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+       
+
+        //honor per sks
+        public int sks()
+        {
+            if (pendidikan! == "S1")
+            {
+                return 20000;
+            }
+            else if (pendidikan! == "S2")
+            {
+                return 25000;
+            }
+            else if (pendidikan! == "S3")
+            {
+                return 30000;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //total honor mengajar
+        public int tHonorMengajar()
+        {
+            if (sks() == 20000)
+            {
+                return inputSKS * sks();
+            }
+            else if (sks() == 25000)
+            {
+                return inputSKS * sks();
+            }
+            else if (sks() == 30000)
+            {
+                return inputSKS * sks();
+            }
+            else
+            {
+                return inputSKS * 0;
+            }
+        }
+
+        //total pendapatan
+        public int  totalPendapatan()
+        {
+            return tHonorMengajar() + tFunctional() + uTransport();
+        }
+
+        //jumlah pph
+        public double jPph()
+        {
+            double Pph = 0.02;
+            return totalPendapatan() * Pph;
+        }
+
+        //total yang dibayarkan
+        public double tDibayar ()
+        {
+            return totalPendapatan() - jPph();
+        }
+    }
+}
---
+++ Latihan2/Latihan2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Latihan2.Honor;
+
+namespace Latihan2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mendeklarasikan objek dengan Class 
+            HonorClass objHonor = new HonorClass();
+
+            //index program
+            Console.WriteLine("Aplikasi Penghitugan Honor Pengajar");
+            Console.WriteLine("###################################");
+            Console.Write("Nama Dosen                  : ");
+            Console.ReadLine();
+            Console.Write("Status Dosen (Tetap/Tidak)  : " );
+            objHonor.status = Console.ReadLine();
+            Console.Write("Pendidikan (S1/S2/S3)       : ");
+            objHonor.pendidikan = Console.ReadLine();
+            Console.Write("Jumlah SKS mengajar         : ");
+            objHonor.inputSKS = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("###################################");
+
+            
+            //proses
+            Console.WriteLine("Hasil Penghitugan Honor Pengajar");
+            Console.WriteLine("###################################");
+            Console.WriteLine("Jumlah per SKS          : Rp." + objHonor.sks().ToString("N0"));
+            Console.WriteLine("Tunjangan Fungsional    : Rp." + objHonor.tFunctional().ToString("N0"));
+            Console.WriteLine("Uang Transport          : Rp." + objHonor.uTransport().ToString("N0"));
+            Console.WriteLine("Total Honor Mengajar    : Rp." + objHonor.tHonorMengajar().ToString("N0"));
+            Console.WriteLine("Total Pendapatan        : Rp." + objHonor.totalPendapatan().ToString("N0"));
+            Console.WriteLine("###################################");
+            Console.WriteLine("Jumlah Pph [-2%]        : -Rp." + objHonor.jPph().ToString("N0"));
+            Console.WriteLine("###################################");
+            Console.WriteLine("Total Honor Berdsih     : Rp." + objHonor.tDibayar().ToString("N0"));
+            Console.WriteLine("###################################");
+
+            Console.ReadKey();
+        }
+    }
+}
